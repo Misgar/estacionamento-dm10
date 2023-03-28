e424b64e974de01e82b8faf953546e59cda2004e
Renato A. Oliveira <renato.oliveira34@fatec.sp.gov.br>
2023-03-28 01:32:07 -0300

    Criada classe para listar proprietarios, e listar carros x proprietarios relacionando o proprietario ao carro estacionado.

---
+++ Model/ListaClientes.php
@@ -0,0 +1,46 @@
+<?php
+require_once "../vendor/autoload.php";
+
+Class ListaClientes{
+    
+     function listaClientes()
+     {
+
+        $pdo = new DBConnection;
+
+        $dados = [];
+
+        $sql = $pdo->returnConnection()->prepare("SELECT * FROM proprietarios");
+
+        if($sql -> rowCount() > 0)
+        {
+            $dados = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+            return $dados;
+
+        }
+
+    }
+
+    public function listaClientesCarros()
+    {
+        $pdo = new DBConnection;
+
+        $dados = [];
+
+        $sql = $pdo->returnConnection()->prepare
+        ("
+        SELECT nome, cpf, email, marcaCarro FROM proprietarios, carrosEstacionados WHERE cpf = cpfProprietario
+        ");
+
+        if($sql -> rowCount() > 0)
+        {
+            $dados = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+            return $dados;
+        }
+    }
+
+}
+
+
